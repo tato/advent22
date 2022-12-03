49bab91e71828570ad09e5308fb7926baa653278
pablo <pabtatoramos@gmail.com>
2022-12-03 07:03:36 +0100

    day03
    this is so much cooler actually

--- day03/day03.zig
+++ day03/day03.zig
@@ -3,22 +3,17 @@ const std = @import("std");
 fn solvePartA(input: []const u8) !void {
     var sum: usize = 0;
 
-    var left = std.AutoHashMap(u8, void).init(gpa.allocator());
-
     var i = std.mem.split(u8, input, "\n");
-    loop_lines: while (i.next()) |rucksack| {
-        defer left.clearRetainingCapacity();
+    while (i.next()) |rucksack| {
+        var left: u52 = 0;
+        for (rucksack[0 .. rucksack.len / 2]) |item|
+            left |= @as(u52, 1) << priority(item);
 
-        for (rucksack[0 .. rucksack.len / 2]) |item| {
-            try left.put(item, {});
-        }
+        var right: u52 = 0;
+        for (rucksack[rucksack.len / 2 ..]) |item|
+            right |= @as(u52, 1) << priority(item);
 
-        for (rucksack[rucksack.len / 2 ..]) |item| {
-            if (left.contains(item)) {
-                sum += priority(item);
-                continue :loop_lines;
-            }
-        }
+        sum += @ctz(left & right) + 1;
     }
 
     std.debug.print("[{d}]\n", .{sum});
@@ -27,47 +22,32 @@ fn solvePartA(input: []const u8) !void {
 fn solvePartB(input: []const u8) !void {
     var sum: usize = 0;
 
-    var elf1 = std.AutoHashMap(u8, void).init(gpa.allocator());
-    var elf2 = std.AutoHashMap(u8, void).init(gpa.allocator());
-
     var i = std.mem.split(u8, input, "\n");
-    loop_groups: while (true) {
-        const elf1_rucksack = i.next() orelse break;
-        const elf2_rucksack = i.next().?;
-        const elf3_rucksack = i.next().?;
+    while (i.next()) |rucksack| {
+        var elf1: u52 = 0;
+        for (rucksack) |item|
+            elf1 |= @as(u52, 1) << priority(item);
 
-        defer elf1.clearRetainingCapacity();
-        defer elf2.clearRetainingCapacity();
+        var elf2: u52 = 0;
+        for (i.next().?) |item|
+            elf2 |= @as(u52, 1) << priority(item);
 
-        for (elf1_rucksack) |item| {
-            try elf1.put(item, {});
-        }
+        var elf3: u52 = 0;
+        for (i.next().?) |item|
+            elf3 |= @as(u52, 1) << priority(item);
 
-        for (elf2_rucksack) |item| {
-            if (elf1.contains(item)) {
-                try elf2.put(item, {});
-            }
-        }
-
-        for (elf3_rucksack) |item| {
-            if (elf2.contains(item)) {
-                sum += priority(item);
-                continue :loop_groups;
-            }
-        }
+        sum += @ctz(elf1 & elf2 & elf3) + 1;
     }
 
     std.debug.print("[{d}]\n", .{sum});
 }
 
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-
-fn priority(item: u8) u8 {
+fn priority(item: u8) u6 {
     if (item >= 'a' and item <= 'z') {
-        return item - 'a' + 1;
+        return @truncate(u6, item - 'a');
     }
     if (item >= 'A' and item <= 'Z') {
-        return item - 'A' + 27;
+        return @truncate(u6, item - 'A' + 26);
     }
     std.debug.panic("Invalid item: {d}", .{item});
 }
